a43d48c5a11db38e0a1b49cf5edfbd4267e0b12a
jun <sjune21@daum.net>
2026-04-28 20:48:06 +0900

    작동

--- image_stitching.py
+++ image_stitching.py
@@ -11,65 +11,71 @@ imgs = [img1, img2, img3, img4]
 
 # Retrieve matching points
 fdetector = cv.BRISK_create()
-keypoints1, descriptors1 = fdetector.detectAndCompute(img1, None)
-keypoints2, descriptors2 = fdetector.detectAndCompute(img2, None)
-keypoints3, descriptors3 = fdetector.detectAndCompute(img3, None)
-keypoints4, descriptors4 = fdetector.detectAndCompute(img4, None)
-
 fmatcher = cv.DescriptorMatcher_create('BruteForce-Hamming')
-match12 = fmatcher.match(descriptors1, descriptors2)
-match23 = fmatcher.match(descriptors2, descriptors3)
-match34 = fmatcher.match(descriptors3, descriptors4)
-match41 = fmatcher.match(descriptors4, descriptors1)
+for i, img in enumerate(imgs):
+    if i is 0:
+        img_merged = img
+    else:
+        keypoints_merged, descriptors_merged = fdetector.detectAndCompute(img_merged, None)
+        keypoints, descriptors = fdetector.detectAndCompute(img, None)
+        match = fmatcher.match(descriptors_merged, descriptors)
 
-# Calculate planar homography and merge two images
-pts1, pts2 = [], []
-for i in range(len(match12)):
-    pts1.append(keypoints1[match12[i].queryIdx].pt)
-    pts2.append(keypoints2[match12[i].trainIdx].pt)
-pts1 = np.array(pts1, dtype=np.float32)
-pts2 = np.array(pts2, dtype=np.float32)
+        # Calculate planar homography and merge two images
+        pts1, pts2 = [], []
+        for i in range(len(match)):
+            pts1.append(keypoints_merged[match[i].queryIdx].pt)
+            pts2.append(keypoints[match[i].trainIdx].pt)
+        pts1 = np.array(pts1, dtype=np.float32)
+        pts2 = np.array(pts2, dtype=np.float32)
 
-H, inlier_mask = cv.findHomography(pts2, pts1, cv.RANSAC)
+        H, inlier_mask = cv.findHomography(pts2, pts1, cv.RANSAC)
 
-# img1의 네 모서리 좌표 (좌상, 좌하, 우하, 우상)
-h, w = img1.shape[:2]
-corners = np.array([[0, 0], [0, h], [w, h], [w, 0]], dtype='float32').reshape(-1, 1, 2)
-# H를 이용해 이 모서리들이 img2 좌표계에서 어디에 찍히는지 계산 (Perspective Transform)
-transformed_corners = cv.perspectiveTransform(corners, H)
-# x, y좌표 구분
-x_coords = transformed_corners[:, 0, 0]
-y_coords = transformed_corners[:, 0, 1]
+        # img의 네 모서리 좌표 (좌상, 좌하, 우하, 우상)
+        h, w = img.shape[:2]
+        corners = np.array([[0, 0], [0, h], [w, h], [w, 0]], dtype='float32').reshape(-1, 1, 2)
+        # H를 이용해 이 모서리들이 img2 좌표계에서 어디에 찍히는지 계산 (Perspective Transform)
+        transformed_corners = cv.perspectiveTransform(corners, H)
+        # x, y좌표 구분
+        x_coords = transformed_corners[:, 0, 0]
+        y_coords = transformed_corners[:, 0, 1]
 
-min_x, max_x = np.min(x_coords), np.max(x_coords)
-min_y, max_y = np.min(y_coords), np.max(y_coords)
-
-
-dx = 0
-dy = 0
-
-if min_x < 0:
-    dx = int(abs(min_x))
-if min_y < 0:
-    dy = int(abs(min_y))
-# 이동 행렬 생성
-T = np.array([[1, 0, dx], [0, 1, dy], [0, 0, 1]], dtype=np.float32)
-H = T @ H
-
-if dx is 0:
-    dx = int(img2.shape[1]-img1.shape[1] + max_x if img2.shape[1] > img1.shape[1] else 0)
-if dy is 0:
-    dy = int(img2.shape[0]-img1.shape[0] + max_y if img2.shape[0] > img1.shape[0] else 0)
+        min_x, max_x = np.min(x_coords), np.max(x_coords)
+        min_y, max_y = np.min(y_coords), np.max(y_coords)
 
 
-img_merged = cv.warpPerspective(img2, H, (img1.shape[1] + dx + 1, img1.shape[0] + dy + 1)) # Perspective Transform
-img_merged[dy:img1.shape[0]+dy,dx:img1.shape[1]+dx] = img1 # Copy
+        dx = int(max(0, -min_x))
+        dy = int(max(0, -min_y))
+
+        dist_x = int(max(img_merged.shape[1], max_x) + dx)
+        dist_y = int(max(img_merged.shape[0], max_y) + dy)
+
+        # 이동 행렬 생성
+        T = np.array([[1, 0, dx], [0, 1, dy], [0, 0, 1]], dtype=np.float32)
+        H = T @ H
+
+
+        result_img = cv.warpPerspective(img, H, (dist_x, dist_y)) # Perspective Transform
+        # 기존 이미지를 새 캔버스에 배치 (덮어쓰기)
+        # 기존 이미지의 (0,0)은 새 캔버스에서 (t_dist[0], t_dist[1]) 위치임
+        result_img[dy:img_merged.shape[0]+dy,dx:img_merged.shape[1]+dx] = img_merged
+    
+        img_merged = result_img
+#        img_merged[dy:img1.shape[0]+dy,dx:img1.shape[1]+dx] = img1 # Copy
+
+
+
+
+
+
+
+
 
 # Show the merged image
-img_matched = cv.drawMatches(img1, keypoints1, img2, keypoints2, match12, None, None, None,
-                             matchesMask=inlier_mask.ravel().tolist()) # Remove `matchesMask` if you want to show all putative matches
-merge = [img1, img2, img_matched, img_merged]
-for i, result in enumerate(merge):
-    cv.imshow(f'Planar Image Stitching {i+1}', result)
+#img_matched = cv.drawMatches(img1, keypoints1, img2, keypoints2, match12, None, None, None,
+#                             matchesMask=inlier_mask.ravel().tolist()) # Remove `matchesMask` if you want to show all putative matches
+#merge = [img1, img2, img_merged]
+#for i, result in enumerate(merge):
+#    cv.imshow(f'Planar Image Stitching {i+1}', result)
+cv.imshow("result", img_merged)
 cv.waitKey(0)
 cv.destroyAllWindows()
